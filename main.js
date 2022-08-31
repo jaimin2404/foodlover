facd611ad234561fbc6573181bb1b7a563d7f3e4
jaimin2404 <prajapatijaimin2404@gmail.com>
2022-08-31 14:52:02 +0530

    start project

---
+++ main.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    
+    $('#menu').click(function(){
+        $(this).toggleClass('fa-times');
+        $('.navbar').toggleClass('nav-toggle')
+    });
+    $(window).on('load scroll',function(){
+        $('#menu').removeClass('fa-times');
+        $('.navbar').removeClass('nav-toggle');
+
+        if($(window).scrollTop() > 60){
+            $('header nav').addClass('header-active');
+        }
+        else{
+            $('header nav').removeClass('header-active');
+        }
+
+        $('section').each(function(){
+            let height = $(this).height();
+            let top = $(window).scrollTop();
+            let offset = $(this).offset().top - 200;
+            let id = $(this).attr('id');
+            if(top >= offset && top < offset + height){
+                $('.navbar ul li a').removeClass('active');
+                $('.navbar').find(`href="#${id}"`).addClass('active');
+            }
+        });
+    });
+
+
+    $('.container .buttons').click(function(){
+
+        $(this).addClass('button-active').siblings().removeClass('button-active');
+        let filter = $(this).attr('data-filter');
+        if(filter == 'all'){
+            $('.dish .image').show(400);
+        }
+        else{
+            $('.dish .image').not('.'+filter).hide(200);
+            $('.dish .image').filter('.'+filter).show(400);
+        }
+
+    });
+});
